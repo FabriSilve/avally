2269945b9f3f1dd899727db4255795a9f9125042
Fabrizio Silvestri <fabri.silve.fs@gmail.com>
2023-05-29 22:15:22 +0200

    Add amplify configs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Amplify } from 'aws-amplify';
+
 import logo from './logo.svg';
 import './App.css';
+
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
